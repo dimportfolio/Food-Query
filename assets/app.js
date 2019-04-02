b1cd217786f7ca46941def02bb800dde42a91f40
dimprofile <danielimbsc@gmail.com>
2019-04-02 14:57:59 -0400

    made a workaround to generating food labels based on ingredients populated

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,15 @@ Then once an img is clicked, generate the title and instruction text
 */
 
 //this function grabs the recipe images with the searched ingredient item.
+// $.ajax({
+//   type: 'GET',
+//   url: 'https://api.nutritionix.com/v1_1/item/taco?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=2621c4e5&appKey=fb83196912d59b7171ffb6f320d7a0fa',
+// }).then(function(test){
+//   console.log(test);
+// })
+
+//curl -v  -X GET "https://api.nutritionix.com/v1_1/item?id=c640834927576f2c7fe01c19&appId=2621c4e5&appKey=fb83196912d59b7171ffb6f320d7a0fa"
+
 $("#submit").on("click", function(){
   var searchVal = $("#search").val();
   var queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?offset=0&query=" + searchVal;
@@ -18,11 +27,11 @@ $("#submit").on("click", function(){
         withCredentials: false
       },
       headers: {
+        
         "X-RapidAPI-Key" : "36b1955be7msh1566b339f906515p10b495jsn27ecc72ce407"
       }
       }).then(function(res) {
-        //This empty method is here incase a user searches for another recipe using an ingredient
-        $("#recipeSection").empty();
+        console.log(res);
         //This is a loop that is generating the recipe images
         for (i=0; i<res.results.length; i++){
           var img = $("<img>");
@@ -49,6 +58,7 @@ function displayRecipe(){
         "X-RapidAPI-Key" : "36b1955be7msh1566b339f906515p10b495jsn27ecc72ce407"
       }
       }).then(function(res) {
+        console.log(res);
         //Once the img is selected, this clears the recipeSection, then appends the title and instructions to it
         $("#recipeSection").empty();
         //create header tag, and adding title text to it
@@ -59,10 +69,76 @@ function displayRecipe(){
         p.text(res.instructions);
         $("#recipeSection").append(h);
         $("#recipeSection").append(p);
-      });
+        //this section creates an array, and has a loop that pushes each ingredient into that array
+        var ingredientArray = [];
+        for (i=0;i<res.extendedIngredients.length; i++){
+        ingredientArray.push(res.extendedIngredients[i].name);
+        console.log(ingredientArray); 
+        var div = $("<div>");
+        div.attr("id", res.extendedIngredients[i].name);
+        div.addClass("labels");
+        div.text(res.extendedIngredients[i].name);
+        $("#nutrition").append(div);
+        };
+
+        //this section creates an array with ingredient objects...
+        // var recipe = [];
+      });   
 
 }
 
-
+function displayLabel(){
+  var labelId = $(this).attr("id");
+  var queryNutritionUrl = `https://trackapi.nutritionix.com/v2/search/instant?query=${
+            labelId
+          }`;
+          $.ajax({
+            type: 'GET',
+            url: queryNutritionUrl,
+            // contentType: 'application/json',
+            xhrFields: {
+              withCredentials: false
+            },
+            headers: {
+              "x-app-id": "2621c4e5",
+              "x-app-key": "fb83196912d59b7171ffb6f320d7a0fa"
+              // "X-RapidAPI-Key" : "ff276a5d86mshf315d9fdfb9fac2p191618jsnaba7bfcf9b2d"
+              // "X-RapidAPI-Key" : "36b1955be7msh1566b339f906515p10b495jsn27ecc72ce407"
+            }
+            }).then(function(response) {
+              console.log(response);
+              $("#label").nutritionLabel({itemName : response.common[0].food_name}); 
+            });
+            
+}
 //Runs displayRecipe function once a recipeImg class is clicked
 $(document).on("click",".recipeImg", displayRecipe);
+$(document).on("click",".labels", displayLabel);
+
+
+/*
+var queryNutritionUrl = `https://trackapi.nutritionix.com/v2/search/instant?query=${
+            ingredientArray[i]
+          }`;
+          $.ajax({
+            type: 'GET',
+            url: queryNutritionUrl,
+            // contentType: 'application/json',
+            xhrFields: {
+              withCredentials: false
+            },
+            headers: {
+              "x-app-id": "2621c4e5",
+              "x-app-key": "fb83196912d59b7171ffb6f320d7a0fa"
+              // "X-RapidAPI-Key" : "ff276a5d86mshf315d9fdfb9fac2p191618jsnaba7bfcf9b2d"
+              // "X-RapidAPI-Key" : "36b1955be7msh1566b339f906515p10b495jsn27ecc72ce407"
+            }
+            }).then(function(response) {
+              console.log(response);
+              // var name = response.branded[0].food_name;
+              // var calories = JSON.stringify(response.branded[0].nf_calories);
+            });
+            */
+
+            // $("#test"+i).nutritionLabel({itemName : recipe[i].name,
+            // valueCalories : recipe[i].cal}); 
